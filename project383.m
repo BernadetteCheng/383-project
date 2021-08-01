b44e464377ab1c4b0d49c989c3f4d3669560d40b
Yzabelle Go <52092038+yzzy2go@users.noreply.github.com>
2021-07-31 21:48:43 -0400

    second iteration

--- project383.m
+++ project383.m
@@ -1,3 +1,5 @@
+clear
+syms f
 %constants%
 gravity = 9.81;
 rho = 998;
@@ -6,27 +8,33 @@ viscosity = 1.0016e-3;
 roughness = 0.0015/1000;
 length = 32/1000;
 width = 26/1000;
-tube_length = 10/100;
+tube_length = 20/100;
 angle = 1/150;
 
 %initial conditions%
 time = 0;
 height = 8/100;
-friction_factor_upper = 0.01;
-friction_factor_lower = 0.02;
 interval = 1;
+velocity_out = 0;
 
 while height>=0
-    velocity_out = 0;
-    while (friction_factor_lower-friction_factor_upper)>= 0.0001
-        velocity_out = sqrt((2*gravity*height+2*gravity*tube_length/150)/(1+tube_length*ffactor/diameter+0.42*(1-diameter^2/(2*width*length/(width+length)^2))));
+    friction_factor_lower = 0.01;
+    friction_factor_upper = 0.011;
+    while abs(friction_factor_upper-friction_factor_lower)>= 0.0001
+        friction_factor_lower=friction_factor_upper;
+        velocity_out = sqrt((2*gravity*height+2*gravity*tube_length/150)/(1+tube_length*friction_factor_lower/diameter+0.42*(1-diameter^2/(2*width*length/(width+length))^2)));
         Re = (rho*velocity_out*diameter)/viscosity;
         
         colebrook_eqn = 1/sqrt(f) == -2*log(roughness/(diameter*3.7)+2.51/(Re*sqrt(f)));
-        friction_factor = double(solve(eqn, f));
+        friction_factor_upper = double(solve(colebrook_eqn, f));
     end
     volumetric_flow_rate = velocity_out*(pi*diameter^2)/4;
     
     time = time+interval;
     height = height-(volumetric_flow_rate/width*length)*time;
+    disp("volumentric_flow_rate: "+volumetric_flow_rate);
+    disp("velocity_out: "+velocity_out);
+    disp(height + " at time " + time);
 end
+
+disp("time to drain: "+time);
