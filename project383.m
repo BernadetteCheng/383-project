4a185d54fe60e6acbf393988dc8a8d878b2aaae9
Annie Zou <x2zou@edu.uwaterloo.ca>
2021-08-03 17:54:22 -0400

    Update project383.m

--- project383.m
+++ project383.m
@@ -8,14 +8,14 @@ viscosity = 1.0016/1000;
 roughness = 0.0015/1000;
 length = 32/100;
 width = 26/100;
-tube_length = 10/100;
+tube_length = 20/100;
 angle = 1/150;
 t_joint_diameter = 11.1125/1000;
 t_joint_length = 4/100;
 
 %initial conditions%
 time = 0;
-height = 8/100;
+height = 10/100;
 interval = 1;
 velocity_out = 0;
 
@@ -27,28 +27,26 @@ ff_plot = [];
 height_plot = [];
 time_plot = [];
 
-while height>=0
+while height>=0.02
     friction_factor_lower = 0.01;
     friction_factor_upper = 0.015;
     while abs(friction_factor_upper-friction_factor_lower)>= 0.0001
         friction_factor_lower=friction_factor_upper;
         
         %Velocity out of the tube, sans T-joint
-        velocity_out = sqrt((2*gravity*height+2*gravity*tube_length/150)/(1+tube_length*friction_factor_lower/diameter+0.42*(1-diameter^2/(2*width*length/(width+length))^2)));
+        velocity_out = sqrt((2*gravity*height+2*gravity*tube_length/150)/(1+tube_length*friction_factor_lower/diameter+0.5));
         
         %Velocity out of the tube, with T-joint
 %         velocity_out = sqrt((2*gravity*height+2*gravity*tube_length/150)/(1+tube_length*friction_factor_lower/diameter+0.42*(1-diameter^2/(2*width*length/(width+length))^2)+(1-(diameter/t_joint_diameter))+(friction_factor_lower*t_joint_length)/t_joint_diameter));
         
         Re = (rho*velocity_out*diameter)/viscosity;
         
-        if Re >= 4000
-            friction_factor_eqn = 1/sqrt(f) == -2*log(roughness/(diameter*3.7)+2.51/(Re*sqrt(f)));
-            friction_factor_upper = double(solve(friction_factor_eqn, f));
-        elseif Re <= 2300
-            friction_factor_eqn = f == 64/Re;
+        if Re > 2300
+            friction_factor_eqn = 1/sqrt(f) == -2*log10(roughness/(diameter*3.7)+2.51/(Re*sqrt(f)));
             friction_factor_upper = double(solve(friction_factor_eqn, f));
         else
-            friciton_factor_upper = 0.042;
+            friction_factor_eqn = f == 64/Re;
+            friction_factor_upper = double(solve(friction_factor_eqn, f));
         end
     end
     volumetric_flow_rate = velocity_out*(pi*diameter^2)/4;
